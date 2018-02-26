eb06900fe98c91d5a2738b15b07c33f00418cc68
Maxim <trav96@mail.ru>
2018-02-26 13:34:39 +0300

    remove console.log

--- src/components/statusBar.js
+++ src/components/statusBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default class Product extends React.Component{
   render(){
-    console.dir(this.props['data-progress']);
     const width = {
       width:this.props['data-progress'] + '%'
     }
